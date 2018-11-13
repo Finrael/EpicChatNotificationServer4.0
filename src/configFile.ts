90c879c425dc8e4c6403601bf31e78039e3dac31
Ovidio <firosdev@gmail.com>
2018-11-13 13:00:30 -0800

    updated

--- src/configFile.ts
+++ src/configFile.ts
@@ -1,11 +1,11 @@
-export const mongo_serverAddress: string = '192.168.99.100';
-export const mongo_serverPort: string = '8000';
-export const notificationServer_Api_Adress :string = 'http://192.168.99.100:5002/api';
-export const notificationServer_Adress :string  = 'http://192.168.99.100:5002';
-export const authenticationServer_Api_Adress :string = 'http://192.168.99.100:5001/api';
-export const authenticationServer_Adress :string  = 'http://192.168.99.100:5001';
-export const middleServer_Api_Adress :string = 'http://192.168.99.100:5000/api';
-export const middleServer_Adress :string  = 'http://192.168.99.100:5000';
+// export const mongo_serverAddress: string = 'localhost';
+// export const mongo_serverPort: string = '27017';
+// export const notificationServer_Api_Adress :string = 'http://192.168.99.100:5002/api';
+// export const notificationServer_Adress :string  = 'http://192.168.99.100:5002';
+// export const authenticationServer_Api_Adress :string = 'http://192.168.99.100:5001/api';
+// export const authenticationServer_Adress :string  = 'http://192.168.99.100:5001';
+// export const middleServer_Api_Adress :string = 'http://192.168.99.100:5000/api';
+// export const middleServer_Adress :string  = 'http://192.168.99.100:5000';
 
 // export const mongo_serverAddress: string = 'localhost';
 // export const mongo_serverPort: string = '27017';
@@ -16,3 +16,11 @@ export const middleServer_Adress :string  = 'http://192.168.99.100:5000';
 // export const middleServer_Api_Adress :string = 'http://localhost:5000/api';
 // export const middleServer_Adress :string  = 'http://localhost:5000';
 
+export const mongo_serverAddress: string = 'localhost';
+export const mongo_serverPort: string = '27017';
+export const notificationServer_Api_Adress :string = 'http://noth-service-v3:5002/api';
+export const notificationServer_Adress :string  = 'http://noth-service-v3:5002';
+export const authenticationServer_Api_Adress :string = 'http://auth-service-v3:5001/api';
+export const authenticationServer_Adress :string  = 'http://auth-service-v3:5001';
+export const middleServer_Api_Adress :string = 'http://mid-service-v3:5000/api';
+export const middleServer_Adress :string  = 'http://mid-service-v3:5000';
